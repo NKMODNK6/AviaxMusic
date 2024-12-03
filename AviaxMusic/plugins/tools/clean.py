c6437eec103a55caad31b630aee705fc32af009e
masoom70 <ajaykurmi702@gmail.com>
2024-12-04 03:41:01 +0530

    Update clean.py

--- AviaxMusic/plugins/tools/clean.py
+++ AviaxMusic/plugins/tools/clean.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 from pyrogram import filters
-from RessoMusic import app
-from RessoMusic.misc import SUDOERS
+from AviaxMusic import app
+from AviaxMusic.misc import SUDOERS
 
 
 @app.on_message(filters.command("clean") & SUDOERS)
